e8b092e7ca82e01246aa69514cbc45d3c88fc77d
reza.azad <coc.m.reza@gamil.com>
2025-08-16 21:08:59 +0330

    create main layout component

---
+++ src/components/Layout/DashboardLayout.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
+
+  return (
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 flex">
+      <Sidebar 
+        isCollapsed={sidebarCollapsed} 
+        onToggleCollapse={() => setSidebarCollapsed(!sidebarCollapsed)} 
+      />
+      
+      <div className="flex-1 flex flex-col">
+        <Navbar />
+        
+        <main className="flex-1 p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
